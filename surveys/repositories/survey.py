fdb46945337883f2867fb065333ba6eb4b6bd260
DorLex <dorozhinskiysanya@gmail.com>
2024-03-21 23:22:45 +0300

    feat(surveys): add get_where query

--- surveys/repositories/survey.py
+++ surveys/repositories/survey.py
@@ -9,3 +9,7 @@ class SurveyRepository:
     def get_all(self) -> QuerySet:
         surveys: QuerySet = self._model.objects.all()
         return surveys
+
+    def get_where(self, filters: dict) -> QuerySet:
+        surveys: QuerySet = self._model.objects.filter(**filters)
+        return surveys
--- surveys/services/survey.py
+++ surveys/services/survey.py
@@ -1,7 +1,9 @@
 from django.db.models import QuerySet
+from rest_framework.generics import get_object_or_404
 from rest_framework.utils.serializer_helpers import ReturnList, ReturnDict
 
 from surveys.factories.survey import SurveyFactory
+from surveys.models import Survey
 from surveys.repositories.survey import SurveyRepository
 from surveys.serializers.survey import SurveySerializer
 
@@ -14,6 +16,13 @@ class SurveyService:
     def get_all(self) -> ReturnList:
         surveys: QuerySet = self._repository.get_all()
         serializer = self._serializer(surveys, many=True)
+        return serializer.data
+
+    def get_where(self, filters: dict) -> ReturnDict:
+        surveys_qs: QuerySet = self._repository.get_where(filters)
+        survey: Survey = get_object_or_404(surveys_qs)
+        serializer = self._serializer(survey)
+
         return serializer.data
 
     def create(self, data: dict) -> ReturnDict:
