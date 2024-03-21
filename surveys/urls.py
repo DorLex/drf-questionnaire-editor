6b4a9c67241d1d6bedcef831081732399c82bd9e
DorLex <dorozhinskiysanya@gmail.com>
2024-03-21 23:24:49 +0300

    fix(surveys): change Survey view from ApiView to ViewSet

--- surveys/urls.py
+++ surveys/urls.py
@@ -1,9 +1,13 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 
 from surveys.views import survey, link
 
+router = DefaultRouter()
+router.register(r'surveys', survey.SurveyViewSet, basename='surveys')
+
 urlpatterns = [
-    path('surveys/', survey.SurveyAPIView.as_view(), name='surveys'),
+    path('', include(router.urls)),
 
     path('any-link/', link.AnyLinkAPIView.as_view(), name='any_link'),
     path('concrete-link/<int:question_id>', link.ConcreteLinkAPIView.as_view(), name='concrete_link'),
--- surveys/views/survey.py
+++ surveys/views/survey.py
@@ -1,17 +1,31 @@
 from rest_framework.response import Response
 from rest_framework.utils.serializer_helpers import ReturnList, ReturnDict
-from rest_framework.views import APIView
+from rest_framework.viewsets import ViewSet
 
 from surveys.services.survey import SurveyService
 
 
-class SurveyAPIView(APIView):
+class SurveyViewSet(ViewSet):
     survey_service = SurveyService()
 
-    def get(self, request):
+    def list(self, request):
         surveys: ReturnList = self.survey_service.get_all()
         return Response(surveys)
 
-    def post(self, request):
+    def create(self, request):
         survey: ReturnDict = self.survey_service.create(request.data)
         return Response(survey)
+
+    def retrieve(self, request, pk=None):
+        filters = {'pk': pk}
+        survey: ReturnDict = self.survey_service.get_where(filters)
+        return Response(survey)
+
+    def update(self, request, pk=None):
+        pass
+
+    def partial_update(self, request, pk=None):
+        pass
+
+    def destroy(self, request, pk=None):
+        pass
